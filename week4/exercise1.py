bd26bbc839d4e5fbfce6a76a5d3d4c8fe3c826d3
Ben Doherty <ben@notionparallax.co.uk>
2017-02-07 10:58:27 -0800

    week 4 complete first draft

--- week4/exercise1.py
+++ week4/exercise1.py
@@ -89,24 +89,56 @@ def wordy_pyramid():
 
 def json_in_a_van():
     """
-    Get some json from a request
-    parse it and extract values
+    Get some json from a request parse it and extract values.
+    Sign up to https://www.wunderground.com/weather/api/ and get an API key
+    TIP: reading json can someimes be a bit confusing. Use a tool like
+         http://www.jsoneditoronline.org/ to help you see what's going on.
+    TIP: these long json accessors base["thing"]["otherThing"] and so on, can
+         get very long. If you are accessing a thing often, assign it to a
+         variable and then future access will be easier.
     """
+    # TODO: not sure how much of this to gut and how much to leave in here?
+    base = "http://api.wunderground.com/api/"
+    api_key = "cf4d86c9502661ab"
+    country = "AU"
+    city = "Sydney"
+    template = "{base}/{key}/conditions/q/{country}/{city}.json"
+    url = template.format(base=base, key=api_key, country=country, city=city)
+    r = requests.get(url)
+    the_json = json.loads(r.text)
+    obs = the_json['current_observation']
+
+    return {"state": obs["display_location"]["state"],
+            "latitude": obs['observation_location']["latitude"],
+            "longitude": obs['observation_location']["longitude"],
+            "local_tz_offset": obs['local_tz_offset']}
 
 
 def diarist():
     """
-    write some data to a file
+    Read in Trispokedovetiles(laser).gcode and count the number of times the
+    laser is turned on and off. That's the command "M10 P1".
+    Write the answer (a number) to a file called 'lasers.pew'
+    TIP: you need to write a string, so you'll need to cast your number
+    TIP: Trispokedovetiles(laser).gcode uses windows style line endings. CRLF
+         not just LF like unix does now. If your comparison is failing this
+         might be why. Try in rather than == and that might help.
+    TIP: remember to commit 'lasers.pew' and push it to your repo, otherwise
+         the test will have nothing to look at.
     """
+    laserCounter = 0
+    with open(LOCAL + "/Trispokedovetiles(laser).gcode") as fp:
+        for line in fp:
+            if "M10 P1" in line:
+                laserCounter += 1
 
-
-def finding_the_gcode():
-    """
-    Parse a file, find the lines that contain a certain
-    value - gcode maybe?
-    """
+    f = open(LOCAL + '/lasers.pew', 'w')
+    f.write(str(laserCounter))
+    f.close()
 
 
 if __name__ == "__main__":
     print [len(w) for w in wordy_pyramid()]
     print get_some_details()
+    print json_in_a_van()
+    print diarist()
--- week4/tests.py
+++ week4/tests.py
@@ -9,9 +9,10 @@ import sys
 import os
 import exercise1
 sys.path.append(os.path.dirname(__file__)[:-5])
-from codeHelpers import test, test_flake8, completion_message
+from codeHelpers import test, test_flake8, completion_message, nyan_cat
 
 WEEK_NUMBER = 4
+LOCAL = os.path.dirname(os.path.realpath(__file__))
 
 
 def syntax_error_message(e):
@@ -41,13 +42,27 @@ lengths = [3, 5, 7, 9, 11, 13, 15, 17, 19, 20, 18, 16, 14, 12, 10, 8, 6, 4]
 testResults.append(
     test([len(w) for w in exercise1.wordy_pyramid()] == lengths,
          "Exercise 1: request some simple data from the internet"))
+weather_results = {'latitude': u'-33.924206',
+                   'state': u'NSW',
+                   'longitude': u'151.187912',
+                   'local_tz_offset': u'+1100'}
+testResults.append(
+    test(exercise1.json_in_a_van() == weather_results,
+         "Exercise 1: get some data from the weather underground."))
 
-# testResults.append(
-#     test(ex1runs(),
-#          "Exercise 1: EXPLAIN TEST HERE"))
+if os.path.isfile(LOCAL + "/lasers.pew"):
+    testResults.append(
+        test(open(LOCAL + "/lasers.pew").read() == "6",
+             "Exercise 1: count the lasers."))
+else:
+    testResults.append(False)
+    print "can't find lasers.pew, did you make it with exactly that file name?"
+
+
 
 print "{0}/{1} (passed/attempted)".format(sum(testResults), len(testResults))
 
 if sum(testResults) == len(testResults):
+    nyan_cat()
     message = "Rad, you've got all the tests passing!"
     completion_message(message, len(message) + 2)
