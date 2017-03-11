d729d888fc825760cb77d75470dc749fcc03b034
Ben Doherty <ben@notionparallax.co.uk>
2017-03-11 18:05:40 +1100

    pass all linter tests

--- week2/exercise1.py
+++ week2/exercise1.py
@@ -7,15 +7,21 @@ Make sure that your code still runs without any errors by pressing
 """
 from __future__ import division
 from __future__ import print_function
-print  ("hello! Let's get started")
-jobs=['get','this',
-'file','to','pass',
-  'the','linter']
-InOtherWords="make it show no linter errors"
 import os
+
+print("hello! Let's get started")
+jobs = ['get', 'this',
+        'file', 'to', 'pass',
+        'the', 'linter']
+InOtherWords = "make it show no linter errors"
 print(jobs)
 print(InOtherWords)
-print(1+1,"is smaller than",7*0.5,"is",(1+1)<(7*0.5),", which is a relief!")
-def usefulFunction () :
+print(1+1, "is smaller than", 7*0.5, "is",
+      (1+1) < (7*0.5), ", which is a relief!")
+
+
+def usefulFunction():
     print(os.getcwd())
-usefulFunction( )
+
+
+usefulFunction()
