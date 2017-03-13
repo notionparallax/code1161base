3373cf1d38dfbf2ef705c44151f72c0ec697efc9
Ben Doherty <ben@notionparallax.co.uk>
2017-03-13 11:27:46 +1100

    make a new patch file

---
+++ patch.sh
@@ -0,0 +1,25 @@
+# edit these lines
+MYNAME="Ben Doherty"
+MYEMAIL="ben@notionparallax.co.uk"
+# stop editing
+
+
+# don't edit these lines
+git config --global user.name $MYNAME
+git config --global user.email $MYEMAIL
+git config --global credential.helper 'cache --timeout=36000' #cache password for 150 minutes
+git config --global color.ui auto #colour the output in git
+git config --global core.editor "atom --wait"
+
+git config --list
+
+apm install script
+apm install linter
+apm install linter-flake8
+
+sudo pip install colorama
+
+sudo pip uninstall flake8 -y
+sudo pip install flake8==3.2.0
+
+echo "That should have fixed the bugs"
