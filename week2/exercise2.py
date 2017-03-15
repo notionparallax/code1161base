7a7a7c4546b25d12923a25c7c1eb8ecd968631d0
Alec <jeremysaguinsin@gmail.com>
2017-03-15 19:06:09 +1100

    Finished? the debug

--- week2/exercise2.py
+++ week2/exercise2.py
@@ -17,14 +17,14 @@ def getLetter(index):
 
 def week2exersise2():
     indices = [12, 2, 26, 7, 0, 12, 12, 4, 17]
-wordArray = map(getLetter, indices)
-wordArray[0] = wordArray[0].upper()
-wordArray[1] = wordArray[1].upper()
-wordArray[3] = wordArray[3].upper()
-secret_word = "".join(wordArray)
-print(secret_word)
-return secret_word
+    wordArray = map(getLetter, indices)
+    wordArray[0] = wordArray[0].upper()
+    wordArray[1] = wordArray[1].upper()
+    wordArray[3] = wordArray[3].upper()
+    secret_word = "".join(wordArray)
+    print(secret_word)
+    return secret_word
 
 
 if __name__ == "__main__":
-    prin(week2exersise2())
+    print(week2exersise2())
