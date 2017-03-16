68281dc5206bfd8d25717d39afba93252a4f1e1d
Alec <jeremysaguinsin@gmail.com>
2017-03-17 08:35:18 +1100

    Did all but one

--- week2/exercise3.py
+++ week2/exercise3.py
@@ -244,10 +244,7 @@ def loops_7():
     This is a hard problem. Use lots of experimentation and draw
     lots of diagrams!
     """
-    star_pyramid = int(input(5))
-
-    for i in range(1, star_pyramid + 1):
-        print((star_pyramid - 1) * "" + i * "*")
+    star_pyramid = []
 
 
 def lp(some_kind_of_list, exercise_name):
