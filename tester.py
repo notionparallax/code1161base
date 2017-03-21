f56c03d653455b0e7dc41c19d9c1a05bb1e0a1f9
Ben <ben@notionparallax.co.uk>
2017-03-21 14:59:07 +1100

    File that tests the testing workflow
    does it in a lightweight way to seed up the loop

---
+++ tester.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+import subprocess
+import os
+
+LOCAL = os.path.dirname(os.path.realpath(__file__))
+week_number = 2
+
+repos = ["pennypangCODE", "wukaicharlott"]
+results = []
+for name in repos:
+    subprocess.call(['python',
+                     './test_shim.py',
+                     "week{}.tests".format(week_number),
+                     "../code1161StudentRepos/{}".format(name)])
+
+    temp_results = open(os.path.join(LOCAL, 'temp_results.json'), 'r')
+    results.append(temp_results.read())
+    temp_results.close()
+
+print results
