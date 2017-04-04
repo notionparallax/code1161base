af07e00b0467ccb65d3bbeaba9df15965c8a8246
Ben <ben@notionparallax.co.uk>
2017-04-05 09:33:11 +1000

    add timeout

--- tester.py
+++ tester.py
@@ -2,6 +2,7 @@
 """Run a lightweight test set on specific students."""
 import subprocess
 import os
+from codeHelpers import Timeout
 
 LOCAL = os.path.dirname(os.path.realpath(__file__))
 week_number = 2
@@ -9,13 +10,14 @@ week_number = 2
 repos = ["pennypangCODE", "wukaicharlott"]
 results = []
 for name in repos:
-    subprocess.call(['python',
-                     './test_shim.py',
-                     "week{}.tests".format(week_number),
-                     "../code1161StudentRepos/{}".format(name)])
+    with Timeout(5):  # should catch any rogue ∞ loops
+        subprocess.call(['python',
+                         './test_shim.py',
+                         "week{}.tests".format(week_number),
+                         "../code1161StudentRepos/{}".format(name)])
 
-    temp_results = open(os.path.join(LOCAL, 'temp_results.json'), 'r')
-    results.append(temp_results.read())
-    temp_results.close()
+        temp_results = open(os.path.join(LOCAL, 'temp_results.json'), 'r')
+        results.append(temp_results.read())
+        temp_results.close()
 
 print results
