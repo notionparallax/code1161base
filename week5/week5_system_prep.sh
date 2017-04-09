7fbfd27e40d30fd033ebb958c04c6ec8d23f2652
Ben <ben@notionparallax.co.uk>
2017-04-09 23:27:55 +1000

    Initial for this

---
+++ week5/week5_system_prep.sh
@@ -0,0 +1,32 @@
+git config --global credential.helper 'cache --timeout=36000' #cache password for 150 minutes
+git config --global color.ui auto #colour the output in git
+git config --global core.editor "atom --wait"
+
+sudo pip install pydocstyle -y
+sudo pip install colorama
+sudo pip uninstall flake8 -y
+sudo pip install flake8
+sudo pip install pydocstyle
+
+apm upgrade
+apm install autocomplete-python
+apm install file-icons
+apm install highlight-selected
+apm install linter
+apm install linter-flake8
+apm install linter-pydocstyle
+apm install markdown-writer
+apm install minimap
+apm install minimap-highlight-selected
+apm install monokai-seti
+apm install open-recent
+apm install pigments
+apm install script
+apm install script
+apm install seti-ui
+apm install sort-lines
+apm install todo-show
+apm install zen
+
+echo
+echo "Everything should be good now!"
