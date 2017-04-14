9aacc747c9ed6cf9fe6254e6ebdc91e4c3e9a946
Ben <ben@notionparallax.co.uk>
2017-04-14 16:08:09 +1000

    Add timing

--- tester.py
+++ tester.py
@@ -1,15 +1,19 @@
 # -*- coding: UTF-8 -*-
 """Run a lightweight test set on specific students."""
-import os
 from codeHelpers import RunCmd
+import os
+import time
 
 LOCAL = os.path.dirname(os.path.realpath(__file__))
 week_number = 4
 
 
 repos = ["code1161benFork_fully_working_secret_squirel"]
+times = []
+repos = ["alanw410", "bvn-architecture","alanw410", "bvn-architecture","alanw410", "bvn-architecture","alanw410", "bvn-architecture","alanw410", "bvn-architecture"]  # "bvn-architecture"
 results = []
 for name in repos:
+    start_time = time.time()
     try:
         timeout_cap = 25
         args = ['python',
@@ -24,5 +28,9 @@ for name in repos:
         temp_results.close()
     except Exception as e:
         print e
+    elapsed_time = time.time() - start_time
+    print "Time to test:", elapsed_time
+    times.append({"name": name, "time": elapsed_time})
 
 print results
+print times
